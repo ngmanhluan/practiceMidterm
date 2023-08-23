abb914c2edb09f04c21e63e693bdd0c098810866
manhluannguyen <ngmanhluan01@gmail.com>
2023-08-23 11:33:26 +0700

    done

--- search.php
+++ search.php
@@ -2,21 +2,18 @@
 <?php 
 $searchInput = $_POST['searchInput'];
 
-
-// Kiểm tra kết nối
-if (!$conn) {
-    die('Lỗi kết nối: ' . mysqli_connect_error());
-}
-
-$query = "SELECT * FROM products WHERE column LIKE '%$searchInput%'";
+$query = "SELECT * FROM products WHERE `name` LIKE '%$searchInput%'";
 $result = mysqli_query($conn, $query);
 
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo '<p>'.$row['column'].'</p>';
+if ($result) {
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo '<p>'.$row['name'].'</p>';
+        }
+    } else {
+        echo 'No results found.';
     }
 } else {
-    echo 'No results found.';
+    echo 'Query error: ' . mysqli_error($conn);
 }
-
-// Đóng kết nối
+?>
